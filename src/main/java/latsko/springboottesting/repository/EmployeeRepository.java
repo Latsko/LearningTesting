bb58f277b944569507862054258f06e9d5346e08
Stanislav Latsko <01latsko10@gmail.com>
2024-03-25 18:51:10 +0100

    commit - initial commit

---
+++ src/main/java/latsko/springboottesting/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package latsko.springboottesting.repository;
+
+import latsko.springboottesting.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+}
---
+++ src/test/java/latsko/springboottesting/UnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package latsko.springboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
